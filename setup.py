0c0e0564445810d4408cafd6cd66ec0e5952179c
joniumGit <52005121+joniumGit@users.noreply.github.com>
2022-02-17 15:42:49 +0200

    fix package mishap by bumping version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='httpheaders',
-    version='1.0.0',
+    version='1.0.1',
     description='HTTP Header Service',
     long_description=long_description,
     long_description_content_type='text/markdown',
