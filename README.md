b97fde3da5a6621c8b4219aaac94e4a2ed1e2242
joniumGit <52005121+joniumGit@users.noreply.github.com>
2022-02-17 15:24:29 +0200

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# headers
-Python library for headers
+# Headers
+
+Python library for HTTP headers.
+
+Contains the standard HTTP headers in a single package for easy importing.
+
+Header definitions are from the IANA site
+https://www.iana.org/assignments/http-fields/http-fields.xhtml
---
+++ setup.py
@@ -0,0 +1,39 @@
+import pathlib
+
+from setuptools import setup, find_packages
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='headers',
+    version='1.0.0',
+    description='HTTP Header Service',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/joniumGit/headers',
+    author='joniumGit',
+    # https://pypi.org/classifiers/
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development',
+    ],
+    keywords='HTTP, headers, API',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    python_requires='>=3',
+    install_requires=[],
+    extras_require={
+        'test': [
+            'pytest',
+            'pytest-cov'
+        ],
+    },
+    project_urls={
+        'Bug Reports': 'https://github.com/joniumGit/headers',
+        'Source': 'https://github.com/joniumGit/headers',
+    },
+)
