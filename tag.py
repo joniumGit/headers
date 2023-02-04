82eaa2460fe5b51cdb893d709afde0f9e58a60f1
joniumGit <52005121+joniumGit@users.noreply.github.com>
2023-02-04 19:59:58 +0200

    Adds date to autogenerate release

--- tag.py
+++ tag.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
         git config --global user.name 'joniumGit'
         git config --global user.email '52005121+joniumGit@users.noreply.github.com'
         git add .
-        git commit -m "Automatically generated release" || echo 'no changes to commit'
+        git commit -m "Automatically generated release {created_at.strftime("%Y.%m.%d")}" || echo 'no changes to commit'
         git push || echo 'no changes to commit'
         git tag {created_at.strftime("%Y.%m.%d")}
         git push --tags || echo 'no changes to commit'
